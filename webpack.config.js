2c788e9dd1e3acbf877147917970f503e20209b7
gorimori <34891695+gorimori@users.noreply.github.com>
2020-01-27 00:52:11 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+/** @typedef {import('webpack').Configuration} Configuration */
+/** @type Configuration */
+module.exports = {
+  name: 'modern',
+  mode: 'development',
+  entry: {
+    main: path.resolve('src', 'ts', 'entry_points', 'main.tsx')
+  },
+  output: {
+    path: path.resolve('public', 'common', 'js'),
+    filename: '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: path.join('public', 'common', 'js', 'shared', 'main'),
+      chunks: 'all'
+    }
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+};
