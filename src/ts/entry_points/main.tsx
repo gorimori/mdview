0589078e218c1c7af4052e8cd4ab7c655b8f8b11
gorimori <34891695+gorimori@users.noreply.github.com>
2020-01-28 00:30:38 +0900

    basic css

--- src/ts/entry_points/main.tsx
+++ src/ts/entry_points/main.tsx
@@ -27,13 +27,19 @@ const App = () => {
         </div>
 
         <div class="c-col-2__col">
-          <h2 class="c-heading -lv2">Output</h2>
+          <div class="p-col-push">
+            <div class="p-col-push__el">
+              <h2 class="c-heading -lv2">Output</h2>
+            </div>
 
-          <div class="p-output-area">
-            <div
-              class="content"
-              dangerouslySetInnerHTML={{ __html: marked(inputValue) }}
-            ></div>
+            <div class="p-col-push__el -push">
+              <div class="p-output-area u-full-height">
+                <div
+                  class="content"
+                  dangerouslySetInnerHTML={{ __html: marked(inputValue) }}
+                ></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
